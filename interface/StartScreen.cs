fd6b8c20401a3d4a4be23224a36c9c9c66e4a12e
pGabrielVP <paulogabrielprado@hotmail.com>
2026-03-19 09:45:01 -0300

    Adiciona localização para a nova cena
    Adiciona localização para as strings adicionadas com a nova cena interface/StartScreen

--- interface/StartScreen.cs
+++ interface/StartScreen.cs
@@ -1,9 +1,14 @@
 using Godot;
+using System.Runtime.InteropServices.JavaScript;
 
 public partial class StartScreen : Control
 {
     [Export]
     PackedScene _jogo;
+    [Export]
+    Button _inicar;
+    [Export]
+    Button _sair;
     public void OnIniciarJogoPressed()
     {
         GetTree().ChangeSceneToFile(_jogo.ResourcePath);
